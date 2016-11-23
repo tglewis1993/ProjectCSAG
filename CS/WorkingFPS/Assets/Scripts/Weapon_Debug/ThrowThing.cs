335259f4f220f1c3c43f5becaa7dbd820b799e2e
tglewis1993 <tglewis1993@gmail.com>
2016-11-23 16:21:06 +0000

    Added grenade throw
    Right click to use

---
+++ CS/WorkingFPS/Assets/Scripts/Weapon_Debug/ThrowThing.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowThing : MonoBehaviour {
+
+    public GameObject thingPrefab;
+    private Transform myTran;
+
+    public float throwForce = 50.0f;
+	// Use this for initialization
+	void Start () {
+
+        myTran = transform;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.GetButtonDown("Fire2"))
+        //{
+
+
+            if(Input.GetButtonUp("Fire2"))
+                throwThing();
+
+
+        //}
+           
+
+	}
+
+    void throwThing()
+    {
+        GameObject thing = (GameObject)Instantiate(thingPrefab, myTran.TransformPoint(-0.2f, 0, 0.5f), myTran.rotation);
+
+        thing.GetComponent<Rigidbody>().AddForce(myTran.forward * throwForce, ForceMode.Impulse);
+
+    }
+}
--- CS/WorkingFPS/Assets/Scripts/Weapon_Debug/WeaponFire.cs
+++ CS/WorkingFPS/Assets/Scripts/Weapon_Debug/WeaponFire.cs
@@ -3,33 +3,53 @@ using System.Collections;
 
 public class WeaponFire : MonoBehaviour {
 
+    void printDebugInfo()
+    {
+        //Debug.Log("Bullet Count: " + bulletCount);
+        //Debug.Log("Reload Pressed: " + m_ReloadPressed);
+        //Debug.Log("Reload Delay: " + canReload);
+    }
+
 	// Use this for initialization
 	void Start ()
     {
+        PlayerView = GetComponentInParent<Camera>();
+
         playerPosition = Vector3.zero;
         playerViewDir = Vector3.zero;
 
-        positionUpdate();
+        updateRay();
+
+        reloadDelay = 0;
+
+        m_ReloadPressed = false;
 
         canFire = 0;
+        canReload = 0;
         inacc = 0.12993f;
-        fireRate = 20.0f;
+        fireDelay = 20.0f;
+        reloadDelay = 200.0f;
 
+
+        magSize = 30;
+
+        bulletCount = magSize;
 
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        positionUpdate();
         fire();
+        printDebugInfo();
         
+
 	}
 
     void positionUpdate()
     {
-        playerPosition = transform.position;
-        playerViewDir = transform.forward;
+        playerPosition = PlayerView.transform.position;
+        playerViewDir = PlayerView.transform.forward;
     }
 
     void updateRay()
@@ -56,35 +76,82 @@ public class WeaponFire : MonoBehaviour {
         else
             canFire = canFire - 1;
 
-        m_mouseClick = Input.GetAxisRaw("Fire1"); // Get mouse click. When mouse is clicked m_mouseClick == 1;
+        m_mouseClick = Input.GetButton("Fire1"); // Get mouse click. When mouse is clicked m_mouseClick == 1;
 
-        if(m_mouseClick>0) // Don't fire if mouse isn't clicked.
+        if(m_mouseClick) // Don't fire if mouse isn't clicked.
         {
-            if (canFire == 0) // Controls time between shots. The larger fire rate is the longer the time between shots.
-                canFire = fireRate;
-            else return; // Don't fire at all between shots. 
-
-            
-
-            if (Physics.Raycast(bulletPath, out shotHitInfo)) // fire a ray using values obtained in updateRay. 
+            if (canFire == 0 && bulletCount > 0) // Controls time between shots. The larger fire delay is the longer the time between shots.
             {
-                Debug.DrawLine(bulletPath.origin, shotHitInfo.point, Color.red, 3);
-                Debug.Log(shotHitInfo.transform.name);
+                canFire = fireDelay;
+                bulletCount--;
+                if (Physics.Raycast(bulletPath, out shotHitInfo)) // fire a ray using values obtained in updateRay. 
+                {
+
+                    
+                    Debug.DrawLine(bulletPath.origin, shotHitInfo.point, Color.red, 3);
+                    Debug.Log(shotHitInfo.transform.tag);
+
+                    
+
+                }
+            }
+
+        }
+        Reload();
+
+    }
+
+    void Reload()
+    {
+        if (bulletCount != magSize) // Don't reload if at full ammo
+        {
+            if (canReload < 0) // reload delay ticker
+                canReload = 0;
+            else
+                canReload = canReload - 1;
+
+            if (!m_ReloadPressed)
+            {
+
+                m_ReloadPressed = Input.GetButtonDown("Reload");
+                if(m_ReloadPressed)
+                    canReload = reloadDelay;
+            }
+
+            if (m_ReloadPressed)
+            { 
+
+                if (canReload == 0)
+                {
+                    bulletCount = magSize;
+                    m_ReloadPressed = false;
+                }
 
             }
         }
 
-
     }
 
+
+    Camera PlayerView;
+
     Ray bulletPath;
-    float m_mouseClick;
+    bool m_mouseClick;
+    bool m_ReloadPressed;
     RaycastHit shotHitInfo;
     Vector3 playerPosition;
     Vector3 playerViewDir;
 
+    
 
     float inacc;
     static float canFire;
-    float fireRate;
+    float fireDelay;
+
+    int bulletCount;
+    int magSize;
+
+    static float canReload;
+    float reloadDelay; // Time between start of reload and end of reload.
+    
 }
