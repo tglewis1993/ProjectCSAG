5b4f1e04cdb715c7e28b72dee26dd5479868ff1f
tglewis1993 <tglewis1993@gmail.com>
2016-12-03 14:21:47 +0000

    Added start menu

---
+++ CS/WorkingFPS/Assets/Main Menu Scene Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+
+    }
+}
