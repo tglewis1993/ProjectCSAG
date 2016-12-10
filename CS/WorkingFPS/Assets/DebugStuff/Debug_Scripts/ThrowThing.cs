2388c30b45deb52199495d7601773cbad9e5e9fe
tglewis1993 <tglewis1993@gmail.com>
2016-12-10 13:40:59 +0000

    Grenades now take player velocity into account
    movementInfluenceFactor added to the throwing script

--- CS/WorkingFPS/Assets/DebugStuff/Debug_Scripts/ThrowThing.cs
+++ CS/WorkingFPS/Assets/DebugStuff/Debug_Scripts/ThrowThing.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class ThrowThing : MonoBehaviour {
 
     public GameObject thingPrefab;
-    public float thingLifetime = 15.0f;
-    private Transform myTran;
+    public float thingLifetime = 3.0f;
 
-    public float throwForce = 50.0f;
+    public CharacterController player_cc;
+
+    private Transform myTran;
+    [Range(0f,0.1f)]public float movementInfluenceFactor = 0.025f;
+    public float throwForce = 5.0f;
 	// Use this for initialization
 	void Start () {
 
         myTran = transform;
+        player_cc = GameObject.Find("Debug_Character").GetComponent<CharacterController>(); // Grab CC for player velocity.
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,11 +32,13 @@ public class ThrowThing : MonoBehaviour {
         if (Input.GetButtonUp("Fire2") && !Input.GetButton("Fire1")) // Will throw when right click is released and when fire isn't held.
 
         {
-            GameObject thing = Instantiate(thingPrefab, myTran.TransformPoint(-0.2f, 0, 0.5f), myTran.rotation);
+            GameObject thing = Instantiate(thingPrefab, myTran.TransformPoint(-0.0f, 0, 0.4f), myTran.rotation);
 
-            thing.GetComponent<Rigidbody>().AddForce(myTran.forward * throwForce, ForceMode.Impulse);
+            Debug.Log(player_cc.velocity);
 
-            StartCoroutine(Despawn(thing, 3.0f));
+            thing.GetComponent<Rigidbody>().AddForce((myTran.forward + (player_cc.velocity * movementInfluenceFactor)) * throwForce, ForceMode.VelocityChange); //(myTran.forward + (player_cc.velocity*0.05f)
+
+            StartCoroutine(Despawn(thing, thingLifetime));
         }
 
     }
